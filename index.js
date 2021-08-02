eb3174fe1e1dec5bb3849f78c7ea43e32e5eacfb
learnnihongo123 <87597195+Scareletbee@users.noreply.github.com>
2021-08-02 12:42:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function message1() {
+  swal("Hello! Good Morning!");
+}
+
+function message2() {
+  swal("Do you like this page?", {
+    buttons: ["No", "Yes"]
+  });
+}
+
+function message3() {
+  swal("OOps!It's not allowed⚠️!", { buttons: "Do not" });
+}
